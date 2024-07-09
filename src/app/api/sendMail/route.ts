1ca8ab7615b9079b8f8e84767a85191b3a8571b5
hemantakumar01 <135641219+hemantakumar01@users.noreply.github.com>
2024-07-09 20:42:21 +0530

    fdgfgfg

--- src/app/api/sendMail/route.ts
+++ src/app/api/sendMail/route.ts
@@ -2,8 +2,14 @@ import { sendMail } from "@/utils/sendMail";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  const { name, email, number, message, password } = await req.json();
-  console.log(name, email, number, message, password);
-  sendMail({ email, message, name, number });
-  return NextResponse.json({ success: true }, { status: 200 });
+  try {
+    const { name, email, number, message, password } = await req.json();
+    // console.log(name, email, number, message, password);
+    console.log("SendMail1");
+    await sendMail({ email, message, name, number });
+    console.log("SendMail2");
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error: any) {
+    console.log("My Error", error.message);
+  }
 };
